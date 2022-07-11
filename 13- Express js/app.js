4910043ea91d34f72e473e9896e1cab289b10ccc
Bibhushan <thapabibhushan@gmail.com>
2022-07-11 13:21:25 +0545

    Server with Express

--- 13- Express js/app.js
+++ 13- Express js/app.js
@@ -1,22 +1,42 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
+// setup static and middleware
+app.use(express.static('./public'))
+
 app.get('/', (req, res) => {
-    console.log('user hit the resource')
-    res.status(200).send('home page')
+    res.sendFile(path.resolve(__dirname,'./public/index.html'))
 })
 
-app.get('/about', (req, res) => {
-    
-    res.status(200).send('About page')
-})
 
 app.all('*', (req, res) => {
-    res.status(404).send('<h1> resource not found <h1/>')
+    res.status(404).send("Page not found")
 })
 
+
+
+
 app.listen(5000, () => {
-    console.log('serve is listening on port 5000..')
+    console.log('server is listening on port 5000..')
 })
+
+
+
+
+
+// app.get('/', (req, res) => {
+//     console.log('user hit the resource')
+//     res.status(200).send('home page')
+// })
+
+// app.get('/about', (req, res) => {
+    
+//     res.status(200).send('About page')
+// })
+
+// app.all('*', (req, res) => {
+//     res.status(404).send('<h1> resource not found <h1/>')
+// })
 
 
