6a1d48edfa303af21fddcbd14d0b6374c65d5699
Bibhushan <thapabibhushan@gmail.com>
2022-07-11 13:58:02 +0545

    Creating basic api using res.json

---
+++ 13- Express js/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+
+const {products} = require('./data')
+
+// app.get('/', (req,res) => {
+//     res.json(products)
+// })
+
+app.get('/', (req,res) => {
+    res.send('<h1> home page </h1><a href="/api/products">products</a>')
+})
+
+app.get('/api/products', (req,res) => {
+    const newProducts = products.map((product) => {
+        const {id, name, image} = product;
+        return {id, name ,image}
+    })
+   
+    res.json(newProducts)
+}) 
+
+app.get('/api/products/1', (req,res) => {
+    const singleProduct = products.find((product) => product.id ===1)
+
+    res.json(singleProduct)
+}) 
+
+
+app.listen(5000, (req, res) => {
+    console.log('hitting port 5000')
+})
