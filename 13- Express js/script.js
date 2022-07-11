a8d2c46cf44327bf5a94751d15b1610a7f98864b
Bibhushan <thapabibhushan@gmail.com>
2022-07-11 16:50:05 +0545

    authorize

--- 13- Express js/script.js
+++ 13- Express js/script.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const app = express()
 const logger = require('./logger')
+const authorize = require('./authorize')
 
 
 
 // req => middleware => res
+
+
+// 1. use vs route
+// 2. options - our won / express /third party
+
 // app.use(logger)
-app.use('/', logger)
+app.use('/', [logger,authorize])
 
 app.get('/', (req, res) => {
     res.send('Home')
