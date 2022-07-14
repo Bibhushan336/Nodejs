c6d41b430b5db3a6c469e5cafb89babd9ece379c
Bibhushan <thapabibhushan@gmail.com>
2022-07-14 17:11:03 +0545

    Event loop

---
+++ 15/app.js
@@ -0,0 +1,16 @@
+console.log("first")
+
+
+setTimeout(() => {
+    console.log('after two seconds')
+}, 2000)
+
+
+
+
+setTimeout(() => {
+    console.log('after 0 seconds')
+}, 0)
+
+
+console.log('second')
