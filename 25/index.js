a875948ed03d879e299f848f5ca020737445d324
Bibhushan336 <thapabibhushan@gmail.com>
2022-07-27 19:43:35 +0545

    setting up 404 error

--- 25/index.js
+++ 25/index.js
@@ -6,5 +6,16 @@ const app = express()
 const publicPath = path.join(__dirname, 'public')
 
 
-app.use(express.static(publicPath))
+// app.use(express.static(publicPath))
+
+app.get('/', (re,res) => {
+    res.sendFile(`${publicPath}/index.html`)
+})
+
+
+app.get('*', (re,res) => {
+    res.send('404 not found')
+})
+
+
 app.listen(3000)
