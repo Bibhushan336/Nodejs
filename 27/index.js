055866e1b5f04e2a8f176c0940144a1c0fd85031
Bibhushan336 <thapabibhushan@gmail.com>
2022-07-28 14:58:14 +0545

    Middleware

---
+++ 27/index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+
+
+
+
+const app = express()
+
+
+
+const reqFilter=(req,res,next) => {
+        // console.log('reqFilter')
+        if(!req.query.age)
+        {
+            res.send('Please provide age')
+        }
+        else if (req.query.age<18) 
+        {
+            res.send('You can not acess this page')
+        }
+        else {
+            next()
+        }
+        
+}
+
+app.use(reqFilter)
+
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to home page')
+})
+
+
+app.get('/user', (req, res) => {
+    res.send('Welcome to user page')
+})
+
+
+
+
+
+
+
+
+app.listen(3000)
